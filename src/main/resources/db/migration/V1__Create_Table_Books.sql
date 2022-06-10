2ca483d06a283ae983246d4fae7bff2eba277217
Bruno César <bruno.cesar@cilia.com.br>
2022-06-10 11:58:28 -0300

    Add migrations and some dependencies

---
+++ src/main/resources/db/migration/V1__Create_Table_Books.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE book(
+    id          SERIAL PRIMARY KEY,
+    author      varchar(255),
+    launch_date date NOT NULL,
+    price       decimal(65, 2) NOT NULL,
+    title       varchar(255)
+);
