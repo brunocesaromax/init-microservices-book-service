29af6266b94fecc61e244091c5d41bfc1fb0b423
Bruno César <bruno.cesar@cilia.com.br>
2022-06-13 10:15:51 -0300

    Add integration of database and spring data

--- src/main/java/com/microservices/bookservice/controller/BookController.java
+++ src/main/java/com/microservices/bookservice/controller/BookController.java
@@ -1,15 +1,13 @@
 package com.microservices.bookservice.controller;
 
 import com.microservices.bookservice.model.Book;
+import com.microservices.bookservice.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("book-service")
@@ -18,10 +16,17 @@ public class BookController {
     @Autowired
     private Environment environment;
 
+    @Autowired
+    private BookRepository repository;
+
     @GetMapping(value = "/{id}/{currency}")
     public Book getBook(@PathVariable Long id, @PathVariable String currency) {
-        var port = environment.getProperty("local.server.port");
+        var book = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not Found"));
 
-        return new Book(1L, "Nigel Poulton", "Docker Deep Dive", LocalDate.now(), BigDecimal.valueOf(13.7), currency, port);
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port);
+
+        return book;
     }
 }
--- src/main/java/com/microservices/bookservice/model/Book.java
+++ src/main/java/com/microservices/bookservice/model/Book.java
@@ -1,28 +1,43 @@
 package com.microservices.bookservice.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity(name = "book")
 public class Book implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "author", nullable = false, length = 180)
     private String author;
 
+    @Column(name = "launch_date", nullable = false)
     private LocalDate launchDate;
 
+    @Column(nullable = false)
     private BigDecimal price;
 
+    @Column(nullable = false, length = 250)
     private String title;
 
+    @Transient
     private String currency;
 
+    @Transient
     private String environment;
 
     public Book(Long id, String author, String title,
