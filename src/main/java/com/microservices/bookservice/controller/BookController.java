afffc7521900d44d13d2bfe841b82b10d6d071ff
Bruno César <bruno.cesar@cilia.com.br>
2022-06-17 10:18:50 -0300

    Use open feign to communicate with cambio service

--- src/main/java/com/microservices/bookservice/controller/BookController.java
+++ src/main/java/com/microservices/bookservice/controller/BookController.java
@@ -1,17 +1,14 @@
 package com.microservices.bookservice.controller;
 
 import com.microservices.bookservice.model.Book;
+import com.microservices.bookservice.proxy.CambioProxy;
 import com.microservices.bookservice.repository.BookRepository;
-import com.microservices.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -23,7 +20,11 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
-    @GetMapping(value = "/{id}/{currency}")
+    @Autowired
+    private CambioProxy cambioProxy;
+
+    // Usando RestTemplate
+    /*@GetMapping(value = "/{id}/{currency}")
     public Book getBook(@PathVariable Long id, @PathVariable String currency) {
         var book = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Book not Found"));
@@ -38,6 +39,21 @@ public class BookController {
 
         var port = environment.getProperty("local.server.port");
         book.setEnvironment(port);
+        book.setPrice(cambio.getConvertedValue());
+
+        return book;
+    }*/
+
+    // Usando OpenFeign
+    @GetMapping(value = "/{id}/{currency}")
+    public Book getBook(@PathVariable Long id, @PathVariable String currency) {
+        var book = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not Found"));
+
+        var cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
+
+        var port = environment.getProperty("local.server.port");
+        book.setEnvironment(port + " FEIGN");
         book.setPrice(cambio.getConvertedValue());
 
         return book;
---
+++ src/main/java/com/microservices/bookservice/proxy/CambioProxy.java
@@ -0,0 +1,17 @@
+package com.microservices.bookservice.proxy;
+
+import com.microservices.bookservice.response.Cambio;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.math.BigDecimal;
+
+@FeignClient(name = "cambio-service", url = "localhost:8000")
+public interface CambioProxy {
+
+    @GetMapping(value = "/cambio-service/{amount}/{from}/{to}")
+    Cambio getCambio(@PathVariable BigDecimal amount,
+                     @PathVariable String from,
+                     @PathVariable String to);
+}
