e3e3d953efd7fb7391f185c4cc2359a89521a54e
Bruno César <bruno.cesar@cilia.com.br>
2022-06-20 10:39:13 -0300

    Resilience4j Rate Limit and BulkHead features

--- src/main/java/com/microservices/bookservice/controller/FooBarController.java
+++ src/main/java/com/microservices/bookservice/controller/FooBarController.java
@@ -1,12 +1,11 @@
 package com.microservices.bookservice.controller;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,11 +16,14 @@ public class FooBarController {
     @GetMapping("/foo-bar")
 //    @Retry(name = "foo-bar", fallbackMethod = "fallbackMethod")
     //Comando para teste do circuit breaker: $watch -n 0.1 "curl http://localhost:8765/book-service/foo-bar"
-    @CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+    //@CircuitBreaker(name = "default", fallbackMethod = "fallbackMethod")
+//    @RateLimiter(name = "default")
+    @Bulkhead(name = "default")
     public String fooBar() {
         logger.info("Request to foo-bar is received!");
-        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
-        return response.getBody();
+//        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+//        return response.getBody();
+        return "Foo-Bar!!!";
     }
 
     //Pode se customizar vários métodos fallbacks com diferentes exceções e assinaturas diferentes,
