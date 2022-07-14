18dcd400203cf069dfc42be21a7c72fa6f00d49b
Bruno César <bruno.cesar@cilia.com.br>
2022-07-14 10:06:56 -0300

    Add circuit breaker to get book service

--- src/main/java/com/microservices/bookservice/controller/BookController.java
+++ src/main/java/com/microservices/bookservice/controller/BookController.java
@@ -3,19 +3,31 @@ package com.microservices.bookservice.controller;
 import com.microservices.bookservice.model.Book;
 import com.microservices.bookservice.proxy.CambioProxy;
 import com.microservices.bookservice.repository.BookRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Tag(name = "Book endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
+
+    private final Logger logger = LoggerFactory.getLogger(BookController.class);
+
+    private final Map<Long, Book> cache = new HashMap<>();
 
     @Autowired
     private Environment environment;
@@ -49,8 +61,9 @@ public class BookController {
 
     // Usando OpenFeign
     @Operation(summary = "Find a specific book by your ID")
+    @CircuitBreaker(name = "getBookCB", fallbackMethod = "getBookFallBack")
     @GetMapping(value = "/{id}/{currency}")
-    public Book getBook(@PathVariable Long id, @PathVariable String currency) {
+    public ResponseEntity<?> getBook(@PathVariable Long id, @PathVariable String currency) {
         var book = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Book not Found"));
 
@@ -60,6 +73,21 @@ public class BookController {
         book.setEnvironment("Book port: " + port + "\n Cambio port: " + cambio.getEnvironment());
         book.setPrice(cambio.getConvertedValue());
 
-        return book;
+        logger.info("Alimentando cache");
+        cache.put(book.getId(), book);
+
+        return ResponseEntity.ok(book);
+    }
+
+    public ResponseEntity<?> getBookFallBack(Long bookId, String currency, Throwable e) {
+        if (cache.containsKey(bookId)) {
+            return ResponseEntity.ok(cache.get(bookId));
+        } else {
+            Map<String, Object> availableBooks = new LinkedHashMap<>();
+            availableBooks.put("message", "Livros disponíveis no momento:");
+            availableBooks.put("books", cache.values());
+
+            return ResponseEntity.ok(availableBooks);
+        }
     }
 }
