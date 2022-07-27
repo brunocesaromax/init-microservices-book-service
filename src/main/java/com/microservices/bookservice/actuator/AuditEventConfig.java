80291f0b0c88eb3fdeee0a69ef180c7ff918769e
Bruno César <bruno.cesar@cilia.com.br>
2022-07-27 10:25:07 -0300

    Add some spring actuator metrics

---
+++ src/main/java/com/microservices/bookservice/actuator/AuditEventConfig.java
@@ -0,0 +1,15 @@
+package com.microservices.bookservice.actuator;
+
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.boot.actuate.audit.InMemoryAuditEventRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuditEventConfig {
+
+    @Bean
+    public AuditEventRepository auditEventRepository() {
+        return new InMemoryAuditEventRepository();
+    }
+}
